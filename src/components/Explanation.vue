c0006c87a67bba311af8519bd618e6ee6fe6f6e4
Saurya Velagapudi <saurya@gmail.com>
2021-05-19 08:27:20 +0800

    Add a rough language picker and break up messages file into separate locales

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -4,5 +4,21 @@
     <p>{{ $t("explanation.projectSummaryParagraph") }}</p>
     <b-button variant="primary" onclick="document.getElementById('addmemorialform').scrollIntoView({ behavior: 'smooth'})">{{ $t("explanation.makeAMemorialButton") }}</b-button>
   </b-jumbotron>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">
+        {{ lang }}
+      </option>
+    </select>
+  </div>
 </div>
 </template>
+
+<script>
+export default {
+  name: 'locale-changer',
+  data () {
+    return { langs: ['hi', 'bn', 'mr', 'pa', 'ta', 'ml', 'kn', 'gu', 'te', 'en'] }
+  }
+}
+</script>
